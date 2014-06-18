1b452809ddb5582f851caf64784cf2799f38e6e8
JonSmith <jon.smith@selectiveanalytics.com>
2014-06-18 16:20:55 +0100

    Got bais of indeterminate action written

---
+++ SampleWebApp/Controllers/ActionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class ActionController : Controller
+    {
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Indeterminate(int id, string foo)
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            return Json("Message from Post action method.");
+        }
+    }
+}
