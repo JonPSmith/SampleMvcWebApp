7f16253531467b90ed0a9c13bee6ad43a65d0962
JonSmith <jon.smith@selectiveanalytics.com>
2014-07-24 09:18:06 +0100

    Added catching sql errors

--- DataLayer/DataClasses/SampleWebAppDb.cs
+++ DataLayer/DataClasses/SampleWebAppDb.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Validation;
@@ -31,6 +32,15 @@ namespace DataLayer.DataClasses
             {
                 return result.SetErrors(ex.EntityValidationErrors);
             }
+            catch (DbUpdateException ex)
+            {
+                var decodedErrors = TryDecodeDbUpdateException(ex);
+                if (decodedErrors == null)
+                    throw; //it isn't something we understand
+
+                return result.SetErrors(decodedErrors);
+            }
+
             return result.SetSuccessMessage("Successfully added or updated {0} items", numChanges);
         }
 
@@ -46,6 +56,15 @@ namespace DataLayer.DataClasses
             {
                 return result.SetErrors(ex.EntityValidationErrors);
             }
+            catch (DbUpdateException ex)
+            {
+                var decodedErrors = TryDecodeDbUpdateException(ex);
+                if (decodedErrors == null)
+                    throw; //it isn't something we understand
+
+                return result.SetErrors(decodedErrors);
+            }
+
             return result.SetSuccessMessage("Successfully added or updated {0} items", numChanges);
         }
 
@@ -91,6 +110,10 @@ namespace DataLayer.DataClasses
             return base.ValidateEntity(entityEntry, items);
         }
 
+
+        //--------------------------------------------------
+        //private helpers
+
         /// <summary>
         /// This handles going through all the entities that have changed and seeing if they need any special handling.
         /// </summary>
@@ -114,8 +137,37 @@ namespace DataLayer.DataClasses
                 if (trackUpdateClass == null) return;
                 trackUpdateClass.UpdateTrackingInfo();
             }
+        }
 
+        private static readonly Dictionary<int, string> SqlErrorTextDict = new Dictionary<int, string>
+        {
+            {547, "This operation failed because another data entry uses this entry."},         //constraint
+            {2601, "One of the properties is marked as Unique index and there is already an entry with that value."} //cannot insert dup key in index
+        };
 
+        /// <summary>
+        /// This decodes the DbUpdateException. If there are any errors it can
+        /// handle then it returns a list of errors. Otherwise it returns null
+        /// which means rethrow the error as it has not been handled
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns>null if cannot handle errors, otherwise a list of errors</returns>
+        private static IEnumerable<ValidationResult> TryDecodeDbUpdateException(DbUpdateException ex)
+        {
+            if (!(ex.InnerException is System.Data.Entity.Core.UpdateException) ||
+                !(ex.InnerException.InnerException is System.Data.SqlClient.SqlException))
+                return null;
+
+            var sqlException = (System.Data.SqlClient.SqlException)ex.InnerException.InnerException;
+            var result = new List<ValidationResult>();
+            for (int i = 0; i < sqlException.Errors.Count; i++)
+            {
+                var errorNum = sqlException.Errors[i].Number;
+                string errorText;
+                if (SqlErrorTextDict.TryGetValue(errorNum, out errorText))
+                    result.Add(new ValidationResult(errorText));
+            }
+            return result.Any() ? result : null;
         }
     }
 }
