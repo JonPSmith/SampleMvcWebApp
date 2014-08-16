e3faff7d3545503ffbf7640d904d3bbe314fe897
JonSmith <jon.smith@selectiveanalytics.com>
2014-08-16 09:04:54 +0100

    Had left out course

---
+++ DataLayer/DataClasses/Concrete/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.DataClasses.Concrete
+{
+    public class Course
+    {
+
+        public int CourseId { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string MainPresenter { get; set; }
+
+        [MaxLength(2048)]
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int LengthDays { get; set; }
+
+        //---------------------------------------
+        //relationships
+
+        public ICollection<Attendee> Attendees { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, StartDate: {1}, LengthDays: {2}", Name, StartDate, LengthDays);
+        }
+    }
+}
