cb9b249108e0ba09b2ee65f2fbd0d7780cac7ebc
JonSmith <jon.smith@selectiveanalytics.com>
2014-08-18 12:42:34 +0100

    Add missed file

---
+++ SampleWebApp/Models/AttendeeAllListModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleWebApp.Models
+{
+    public class AttendeeAllListModel
+    {
+        [UIHint("HiddenInput")]
+        public int AttendeeId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string FullName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        //This column has been specifically included so we can add a column based security constraint
+        public bool HasPaid { get; set; }
+
+
+    }
+}
