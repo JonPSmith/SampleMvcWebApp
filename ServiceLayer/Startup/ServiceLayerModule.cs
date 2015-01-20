241cc911412a010f9217e03a644e662cdba0eb2a
JonSmith <jon.smith@selectiveanalytics.com>
2015-01-20 12:37:22 +0000

    Tidy up of DI

--- ServiceLayer/Startup/ServiceLayerModule.cs
+++ ServiceLayer/Startup/ServiceLayerModule.cs
@@ -50,30 +50,6 @@ namespace ServiceLayer.Startup
 
             //and register the GenericServices assembly
             builder.RegisterAssemblyTypes(typeof(IListService).Assembly).AsImplementedInterfaces();
-            
-            //builder.RegisterGeneric(typeof(CreateService<>)).As(typeof(ICreateService<>));
-            //builder.RegisterGeneric(typeof(DeleteService<>)).As(typeof(IDeleteService<>));
-            //builder.RegisterGeneric(typeof(DetailService<>)).As(typeof(IDetailService<>));
-            //builder.RegisterGeneric(typeof(ListService<>)).As(typeof(IListService<>));
-            //builder.RegisterGeneric(typeof(UpdateService<>)).As(typeof(IUpdateService<>));
-
-            //builder.RegisterGeneric(typeof(CreateServiceAsync<>)).As(typeof(ICreateServiceAsync<>));
-            //builder.RegisterGeneric(typeof(DeleteServiceAsync<>)).As(typeof(IDeleteServiceAsync<>));
-            //builder.RegisterGeneric(typeof(DetailServiceAsync<>)).As(typeof(IDetailServiceAsync<>));
-            //builder.RegisterGeneric(typeof(UpdateServiceAsync<>)).As(typeof(IUpdateServiceAsync<>));
-
-            //builder.RegisterGeneric(typeof(CreateService<,>)).As(typeof(ICreateService<,>));
-            //builder.RegisterGeneric(typeof(CreateSetupService<,>)).As(typeof(ICreateSetupService<,>));
-            //builder.RegisterGeneric(typeof(DetailService<,>)).As(typeof(IDetailService<,>));
-            //builder.RegisterGeneric(typeof(ListService<,>)).As(typeof(IListService<,>));
-            //builder.RegisterGeneric(typeof(UpdateService<,>)).As(typeof(IUpdateService<,>));
-            //builder.RegisterGeneric(typeof(UpdateSetupService<,>)).As(typeof(IUpdateSetupService<,>));
-
-            //builder.RegisterGeneric(typeof(CreateServiceAsync<,>)).As(typeof(ICreateServiceAsync<,>));
-            //builder.RegisterGeneric(typeof(CreateSetupServiceAsync<,>)).As(typeof(ICreateSetupServiceAsync<,>));
-            //builder.RegisterGeneric(typeof(DetailServiceAsync<,>)).As(typeof(IDetailServiceAsync<,>));
-            //builder.RegisterGeneric(typeof(UpdateServiceAsync<,>)).As(typeof(IUpdateServiceAsync<,>));
-            //builder.RegisterGeneric(typeof(UpdateSetupServiceAsync<,>)).As(typeof(IUpdateSetupServiceAsync<,>));
 
         }
 
