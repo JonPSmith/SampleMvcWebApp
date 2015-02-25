3ea230ba111bc899219705535b1f72bd00a5415d
JonSmith <jon.smith@selectiveanalytics.com>
2015-02-25 15:16:46 +0000

    Optimisted JavaScript/CSS by removing old files no longer used. Also updated MVC and Bootstrap to latest version

--- SampleWebApp/App_Start/BundleConfig.cs
+++ SampleWebApp/App_Start/BundleConfig.cs
@@ -34,18 +34,26 @@ namespace SampleWebApp
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
+
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/ActionRunner").Include(
-                "~/Scripts/jquery-notify.js",
-                "~/Scripts/jquery-ui-{version}.js",
-                "~/Scripts/jquery.signalR-{version}.js",
-                "~/Scripts/ActionRunnerComms.js",
-                "~/Scripts/ActionRunnerUi.js"));
+            //Not used anymore
+            //bundles.Add(new ScriptBundle("~/bundles/ActionRunner").Include(
+            //    "~/Scripts/jquery-notify.js",
+            //    "~/Scripts/jquery-ui-{version}.js",
+            //    "~/Scripts/jquery.signalR-{version}.js",
+            //    "~/Scripts/ActionRunnerComms.js",
+            //    "~/Scripts/ActionRunnerUi.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -58,23 +66,24 @@ namespace SampleWebApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/notify.css",
+                      //"~/Content/notify.css", - not used any more
                       "~/Content/site.css"));
 
-            //Commented out the parts that are not used from the jQuery UI
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-              "~/Content/themes/base/jquery.ui.core.css",
-              "~/Content/themes/base/jquery.ui.resizable.css",
-              "~/Content/themes/base/jquery.ui.selectable.css",
-              //"~/Content/themes/base/jquery.ui.accordion.css",
-              //"~/Content/themes/base/jquery.ui.autocomplete.css",
-              //"~/Content/themes/base/jquery.ui.button.css",
-              "~/Content/themes/base/jquery.ui.dialog.css",
-              //"~/Content/themes/base/jquery.ui.slider.css",
-              //"~/Content/themes/base/jquery.ui.tabs.css",
-              //"~/Content/themes/base/jquery.ui.datepicker.css",
-              "~/Content/themes/base/jquery.ui.progressbar.css",
-              "~/Content/themes/base/jquery.ui.theme.css"));
+            //Not used any more
+            ////Commented out the parts that are not used from the jQuery UI
+            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            //  "~/Content/themes/base/jquery.ui.core.css",
+            //  "~/Content/themes/base/jquery.ui.resizable.css",
+            //  "~/Content/themes/base/jquery.ui.selectable.css",
+            //  //"~/Content/themes/base/jquery.ui.accordion.css",
+            //  //"~/Content/themes/base/jquery.ui.autocomplete.css",
+            //  //"~/Content/themes/base/jquery.ui.button.css",
+            //  "~/Content/themes/base/jquery.ui.dialog.css",
+            //  //"~/Content/themes/base/jquery.ui.slider.css",
+            //  //"~/Content/themes/base/jquery.ui.tabs.css",
+            //  //"~/Content/themes/base/jquery.ui.datepicker.css",
+            //  "~/Content/themes/base/jquery.ui.progressbar.css",
+            //  "~/Content/themes/base/jquery.ui.theme.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
